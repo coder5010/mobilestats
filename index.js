99dc5babaeb8c4e664823c16c1b1ccd53b35fb3a
Armymn6549 <72281712+Armymn6549@users.noreply.github.com>
2020-12-14 17:31:45 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android"
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+// You really don't want your token here since your repl's code
+// is publically available. We'll take advantage of a Repl.it 
+// feature to hide the token we got earlier. 
+client.login(process.env.TOKEN);
